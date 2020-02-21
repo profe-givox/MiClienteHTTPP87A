f16e48ec2e271cb9d5c4343ff54bdf3c950667d9
profe-givox <gi.vega@itsur.edu.mx>
2020-02-21 11:46:51 -0600

    Se agrego peticiones simples con Volley

--- app/src/main/java/net/ivanvega/miclientehttpp87a/MainActivity.java
+++ app/src/main/java/net/ivanvega/miclientehttpp87a/MainActivity.java
@@ -10,6 +10,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity  implements DownloadCallback {
 
 
@@ -28,6 +35,35 @@ public class MainActivity extends AppCompatActivity  implements DownloadCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         networkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://www.google.com");
+
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        Toast.makeText(MainActivity.this,
+                                "Response is: "+ response.substring(0,500),
+                                Toast.LENGTH_SHORT).show();
+                        Log.i("VOLLEYOPS",
+                                "Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                           // textView.setText("That didn't work!");
+                        }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
     }
